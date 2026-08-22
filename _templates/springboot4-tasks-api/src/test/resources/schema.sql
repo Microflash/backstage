80d5a3d323d959deb5ca836f0d857f5d359d9212
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2026-08-22 18:25:23 +0530

    template: update maven-java25-template

--- _templates/springboot4-tasks-api/src/test/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create extension if not exists "uuid-ossp";
-drop table if exists tasks;
-create table tasks (
-	id         uuid primary key      default uuid_generate_v1(),
-	title      text not null,
-	created_by text not null,
-	remind_at  timestamptz
-);
